f3c1c8308b4cad9dbc726b54f1b9f19137390325
Darahakupets <ilyadorogokupets@yahoo.com>
2023-10-27 18:09:31 +0300

    Create project

---
+++ src/main/java/by/dorogokupets/svservice/SvServiceApplication.java
@@ -0,0 +1,13 @@
+package by.dorogokupets.svservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/dorogokupets/svservice/SvServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dorogokupets.svservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
