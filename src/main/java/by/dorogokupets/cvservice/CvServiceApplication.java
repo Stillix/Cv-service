eb5af67c2dfb7d3a0efb42da2c5cf21679f084bb
Darahakupets <ilyadorogokupets@yahoo.com>
2023-11-08 14:22:12 +0300

    add

--- src/main/java/by/dorogokupets/cvservice/CvServiceApplication.java
+++ src/main/java/by/dorogokupets/cvservice/CvServiceApplication.java
@@ -2,7 +2,9 @@ package by.dorogokupets.cvservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
